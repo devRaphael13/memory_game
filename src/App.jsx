b90f938d67d4a0901f95d9f600e114e359b09f9b
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-02-27 12:40:26 +0100

    Feat: add score board

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [pokemon, setPokemon] = useState([]);
+    const [score, setScore] = useState(0);
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
@@ -27,13 +28,16 @@ function App() {
     };
 
     const handleSelect = (e) => {
-        setPokemon(shuffle(pokemon.map((mon) => (e.target.textContent === mon.name ? { ...mon, selected: mon.selected + 1 } : mon))));
+        const name = e.target.textContent;
+        setScore(score + 1)
+        setPokemon(shuffle(pokemon.map((mon) => (name === mon.name ? { ...mon, selected: mon.selected + 1 } : mon))));
     };
 
     return (
         <main>
             <h1>Memory Game</h1>
             <section>
+                <h2>{score}</h2>
                 {pokemon.map((monster) => (
                     <button onClick={handleSelect} data-id={monster.id} key={monster.id}>
                         {monster.name}
